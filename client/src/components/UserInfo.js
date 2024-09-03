b95156f24bdd432947e904b833083388f5102f14
JENNIFER-MMESOMA <169096499+JENNIFER-MMESOMA@users.noreply.github.com>
2024-09-03 08:50:26 -0700

    Update UserInfo.js

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,10 +1,1 @@
-// const UserInfo = () => {
-//   return (
-//     <div>
-//       <h1>Jennifer Mmesoma</h1>
-//       <p>adammesomachi@gmail.com</p>
-//       <a href="">+2349122322926</a>
-//     </div>
-//   );
-// };
-// export default UserInfo;
+
